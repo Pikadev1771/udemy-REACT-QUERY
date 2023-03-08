a654d4412bbfadcd05fd0b9529866567f7c78a3c
Park Kyung Hyun <pikadev1771@gmail.com>
2023-03-08 21:36:02 +0900

    12. Query Key

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -25,8 +25,9 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const { data, isLoading, isError, error } = useQuery('comments', () =>
-    fetchComments(post.id)
+  const { data, isLoading, isError, error } = useQuery(
+    ['comments', post.id],
+    () => fetchComments(post.id)
   );
 
   if (isLoading) return <h3>Loading...</h3>;
